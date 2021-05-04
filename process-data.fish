e08f6f90aeba0a020e24518924d7850af6a564cf
Daniel Goz <dansman805@gmail.com>
2021-05-04 19:27:13 -0400

    Add processing of playoffs and quals

--- process-data.fish
+++ process-data.fish
@@ -45,6 +45,22 @@ jq '.matches | .[] | {"actualStartTime": .actualStartTime,
 "modifiedOn": .modifiedOn
 }' 'matches.json' | jq -s '.' | json_to_csv > 'matches.csv'
 
+set process_scores '.[] | .[] |
+if .alliances == null then .alliances = [{}, {}] else . end
+| .alliances = (.alliances
+| map(.alliance as $allianceColor | del(.alliance) | with_entries(.key = .key + $allianceColor)))
+| .alliance0 = .alliances[0] | .alliance1 = .alliances[1] | del(.alliances)
+| with_entries(if .key=="scores" or .key=="alliance0" or .key=="alliance1" then .value|to_entries[] else . end)'
+# Set alliances to empty objects if it doesn't exist to keep everything after it from breaking.
+# Saving the alliance, remove its key. Then, append it to every key for the alliance objects.
+# Then, elevate the individual alliance objects, then remove the .alliances array
+# Finally, elevate the key/value pairs in scores, alliance0, alliance1, thereby removing them.
+
+jq $process_scores quals.json  | jq -s '.' | json_to_csv > quals.csv
+jq $process_scores playoffs.json  | jq -s '.' | json_to_csv > playoffs.csv
+
 rm 'ftcdata.db'
 sqlite3 'ftcdata.db' -cmd '.mode csv' '.import events.csv events'
 sqlite3 'ftcdata.db' -cmd '.mode csv' '.import matches.csv matches'
+sqlite3 'ftcdata.db' -cmd '.mode csv' '.import quals.csv quals'
+sqlite3 'ftcdata.db' -cmd '.mode csv' '.import playoffs.csv playoffs'
