bca0cacb7ac3a9220fc8b17bd50cd15e35f4f08d
Daniel Goz <daniel@dgoz.net>
2021-07-23 00:02:21 -0400

    Change output path name to data

--- retrieve-data.fish
+++ retrieve-data.fish
@@ -3,28 +3,28 @@
 set BASE_API_URL "https://ftc-api.firstinspires.org/v2.0"
 set YEAR "2020"
 
-mkdir -p output
+mkdir -p data
 
 function query_api
     curl -H 'Accept:application/json' -H "Authorization: Basic $FTC_EVENTS_KEY" "$BASE_API_URL/$YEAR/$argv"
 end
 
-query_api 'events' > 'output/events.json'
+query_api 'events' > 'data/events.json'
 
-set played_events (jq -r '.events | .[] | select(.published) | .code' output/events.json)
+set played_events (jq -r '.events | .[] | select(.published) | .code' data/events.json)
 
 for event in $played_events
     query_api "matches/$event"
-end > output/matches.json
+end > data/matches.json
 
 for event in $played_events
     query_api "scores/$event/qual"
-end > output/quals.json
+end > data/quals.json
 
 for event in $played_events
     query_api "scores/$event/playoff"
-end > output/playoffs.json
+end > data/playoffs.json
 
 for page in (seq (query_api "teams" | jq '.pageTotal'))
     query_api "teams?page=$page"
-end > output/teams.json
+end > data/teams.json
